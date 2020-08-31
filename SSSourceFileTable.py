2e2cc81680e18cf425d32baeebce661ab007c74e
berres2002 <berres2002@gmail.com>
2020-08-30 17:56:37 -0700

    Added/Fixed input schema

--- SSSourceFileTable.py
+++ SSSourceFileTable.py
@@ -17,7 +17,7 @@ class SSSourceBuilder(FileTableBuilder):
                     "AstDec(deg)", "AstDecRate(deg/day)",
                     "Ast-Sun(J2000x)(km)", "Ast-Sun(J2000y)(km)",
                     "Ast-Sun(J2000z)(km)", "Sun-Ast-Obs(deg)",
-                    "V", "FiltermagV(H=0)", "Filter", "AstRASigma(mas)",
+                    "V", "Filtermag", "V(H=0)", "Filter", "AstRASigma(mas)",
                     "AstDecSigma(mas)", "PhotometricSigma(mag)")
 
     def __init__(self, parent: FileTable, input_filename: str,
